f06e6d2ec32037a26902f6fb477c2edf3aa26022
nwin3 <nitin.gehlot2212@gmail.com>
2026-06-18 23:30:12 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 // loader.js
 // loader.js
-fetch('/pages/nav_bar.html')
+fetch('/pages/navbar.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('nav-placeholder').innerHTML = data;
